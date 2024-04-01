395a48c371df2772174846a383e063f64ae73203
onceuponatim9 <sjung8018a@gmail.com>
2024-04-01 18:28:10 +0900

    Update Main.java

--- polyGame/src/polyGame/Main.java
+++ polyGame/src/polyGame/Main.java
@@ -2,7 +2,7 @@ package polyGame;
 
 public class Main {
 	public static void main(String[] args) {
-		GameManager gameManager = new GameManager();
+		GameManager game = new GameManager();
 		while(true) {
 			break;
 		}
