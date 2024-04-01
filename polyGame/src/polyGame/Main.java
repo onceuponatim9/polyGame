d0ffea5989aaea573e0904a9bef1b92115aaf654
onceuponatim9 <sjung8018a@gmail.com>
2024-04-01 17:36:52 +0900

    Initial Commit

---
+++ polyGame/src/polyGame/Main.java
@@ -0,0 +1,13 @@
+package polyGame;
+
+public class Main {
+	public static void main(String[] args) {
+		GameManager gameManager = new GameManager();
+		while(true) {
+			break;
+		}
+		
+		System.out.println("game over-");
+	}
+
+}
