c0f0e388a59d11ca713b7ac634c09d65e16d63ef
onceuponatim9 <sjung8018a@gmail.com>
2024-04-02 17:44:20 +0900

    Update GameManager.java

--- polyGame/src/polyGame/GameManager.java
+++ polyGame/src/polyGame/GameManager.java
@@ -6,16 +6,50 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GameManager {
-	static Scanner scan = new Scanner(System.in);
-	private Random ran = new Random();
+	Scanner scan = new Scanner(System.in);
+	Random ran = new Random();
 	
 	Map<String, Stage> stageList = new HashMap<String, Stage>();
 	
-	static String nextStage = "";
-	private String curStage = "";
+	String nextStage = "";
+	String curStage = "";
+	
+	public boolean isRun = true;
 	
 	public GameManager() {
+		stageList.put("Lobby", new Lobby());
+		stageList.put("Stage1", new Stage1());
+		stageList.put("Stage2", new Stage2());
+		nextStage = "Lobby";
+	}
+	
+	public void changeStage() {
+		System.out.println("curStage : " + curStage);
+		System.out.println("nextStage : " + nextStage);
 		
+		curStage = nextStage;
+		Stage stage = stageList.get(curStage);
+		
+		while(isRun) {
+			Battle battle = new Battle();
+			boolean isUpdate = battle.update(); // 여기가 문제
+			if (!isUpdate)
+				break;
+		}
+		
+		if (nextStage.equals(""))
+			isRun = false;
+	}
+	
+	public int inputNumber() {
+		int number = -1;
+		try {
+			String input = scan.next();
+			number = Integer.parseInt(input);
+		} catch(Exception e) {
+			System.out.println("숫자를 입력하세요.");
+		}
+		return number;
 	}
 
 }
