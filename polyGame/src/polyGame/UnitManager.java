7a3bcabad10383d0121c8a11836886809f890533
onceuponatim9 <sjung8018a@gmail.com>
2024-04-02 12:19:31 +0900

    Update UnitManager.java

--- polyGame/src/polyGame/UnitManager.java
+++ polyGame/src/polyGame/UnitManager.java
@@ -1,8 +1,23 @@
 package polyGame;
 
+import java.util.Vector;
+
 public class UnitManager {
+	Vector<Player> playerList = new Vector<>();
+	Vector<Unit> monsterList = new Vector<>();
+	
 	public UnitManager() {
-		
+		playerList.add(new Player("name1", 1000, 50));
+		playerList.add(new Player("name2", 800, 60));
+		playerList.add(new Player("name3", 600, 70));
+	}
+	
+	public Vector<Player> getPlayerList() {
+		return this.playerList;
+	}
+	
+	public Vector<Unit> getMonsterList() {
+		return this.monsterList;
 	}
 
 }
