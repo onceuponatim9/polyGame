0009608ebc2127a72d9520dd09730cb6d4a9f39a
onceuponatim9 <sjung8018a@gmail.com>
2024-04-02 17:42:57 +0900

    Update Main.java

--- polyGame/src/polyGame/Main.java
+++ polyGame/src/polyGame/Main.java
@@ -3,15 +3,22 @@ package polyGame;
 public class Main {
 	public static void main(String[] args) {
 		GameManager gm = new GameManager();
+		UnitManager um = new UnitManager();
 		Game game = new Game();
 		
+		System.out.println("==== TEXT RPG ====");
+		
+		// 게임 시작
 		while(true) {
 			boolean isStart = game.start();
-			if(isStart)
+			if(isStart) {
 				break;
+			}
 		}
 		
+		// 게임 진행
 		while(true) {
+			um.makeMonster(3);
 			gm.changeStage();
 			if(!gm.isRun)
 				break;
