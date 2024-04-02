fd8c85913c53af0be1053d9266d906872fe21364
onceuponatim9 <sjung8018a@gmail.com>
2024-04-02 11:25:35 +0900

    Update GameManager.java

--- polyGame/src/polyGame/GameManager.java
+++ polyGame/src/polyGame/GameManager.java
@@ -1,6 +1,19 @@
 package polyGame;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
 public class GameManager {
+	static Scanner scan = new Scanner(System.in);
+	private Random ran = new Random();
+	
+	Map<String, Stage> stageList = new HashMap<String, Stage>();
+	
+	static String nextStage = "";
+	private String curStage = "";
+	
 	public GameManager() {
 		
 	}
