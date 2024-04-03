88f187397dbe77ad968ba87ec5da4e0ebe0ad437
onceuponatim9 <154542332+onceuponatim9@users.noreply.github.com>
2024-04-03 18:56:43 +0900

    Update Lobby.java

--- polyGame/src/polyGame/Lobby.java
+++ polyGame/src/polyGame/Lobby.java
@@ -4,27 +4,18 @@ public class Lobby extends Stage {
 
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
-		GameManager gm = new GameManager();
-		
 		System.out.println("=====[Lobby]=====");
 		System.out.println("[1.전투] [2.종료]");
-		int sel = gm.scan.nextInt();
+		int sel = GameManager.scan.nextInt();
 		
 		if(sel == 1) {
-			gm.nextStage = "Stage1";
-			return true;
+			GameManager.nextStage = "BATTLE";
 			
 		} else if(sel == 2) {
-			gm.nextStage = "";
-			gm.isRun = false;
-			return false;
-			
-		} else {
-			gm.nextStage = gm.curStage;
-			return true;
+			GameManager.nextStage = "";
 		}
 		
+		return false;
 	}
 
 }
