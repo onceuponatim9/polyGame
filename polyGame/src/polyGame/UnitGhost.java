d72710d88a6cbfb5473e14bfa3971819ae966656
onceuponatim9 <154542332+onceuponatim9@users.noreply.github.com>
2024-04-03 18:11:36 +0900

    Update and rename UnitBat.java to UnitGhost.java

---
+++ polyGame/src/polyGame/UnitGhost.java
@@ -0,0 +1,14 @@
+package polyGame;
+
+public class UnitGhost extends Unit {
+	public UnitGhost() {
+		name = "ghost";
+	}
+
+	@Override
+	public void skill() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
