b0fb1a3d13db307279c42ef06aeaf4cacc3da87a
onceuponatim9 <154542332+onceuponatim9@users.noreply.github.com>
2024-04-03 19:02:12 +0900

    Create UnitDragon.java

---
+++ polyGame/src/polyGame/UnitDragon.java
@@ -0,0 +1,14 @@
+package polyGame;
+
+public class UnitDragon extends Unit {
+	public UnitDragon() {
+		name = "dragon";
+	}
+	
+	@Override
+	public void skill() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
